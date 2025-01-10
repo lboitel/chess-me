568e2a7e7042cdd092bd132f2b3b7864bb8a074f
Léo-Paul BOITEL <leo-paul.boitel@trimane.fr>
2025-01-10 12:02:14 +0100

    added number games on analysi API

--- chessApi.py
+++ chessApi.py
@@ -1,13 +1,16 @@
 import flask
 from flask import request, jsonify, Flask
 import playerAnalyzer as pa
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Autorise les requêtes cross-origin
 
 import chess
 import chess.engine
 
-engine = chess.engine.SimpleEngine.popen_uci("C:/Users/lboitel/Documents/PC-30-11/perso/chess-me/stockfish/stockfish.exe")
+# engine = chess.engine.SimpleEngine.popen_uci("C:/Users/lboitel/Documents/PC-30-11/perso/chess-me/stockfish/stockfish.exe")
+engine = chess.engine.SimpleEngine.popen_uci("stockfish/stockfish_exe")
 
 # Définir une route de base (GET)
 @app.route('/')
@@ -19,24 +22,22 @@ def home():
 def generate_analysis():
     player_name = request.args.get('player_name')
     print(player_name)
-    # Données simples simulées
     analyzer = pa.PlayerAnalyzer(player_name)
     analyzer.set_pgn_data()
     analyzer.analyse_games(analyzer.pgn_data)
 
-    print(analyzer.analyzed_games.mean(numeric_only=True))
-
-    data = analyzer.analyzed_games.head(10).to_dict()
+    data = analyzer.analyzed_games.mean(numeric_only=True).to_dict()
+    data['number_games'] = analyzer.analyzed_games.shape[0]
 
     engine.configure({
-        "Skill Level": 15,  # Niveau de force
+        "Skill Level": 10,  # Niveau de force
         "UCI_LimitStrength": True,  # Limitation de l'ELO
-        "UCI_Elo": 800,  # ELO cible
-        "UCI_Contempt": 10,  # Privilégier les parties agressives (éviter les nulles)
+        "UCI_Elo": 1330,  # ELO cible
+        # "UCI_Contempt": 10,  # Privilégier les parties agressives (éviter les nulles)
         "Hash": 512,  # 512 Mo pour le cache
-        "Use NNUE": True,  # Utilisation du réseau neuronal pour l'évaluation
+        # "Use NNUE": True,  # Utilisation du réseau neuronal pour l'évaluation
     })
-
+    
     return jsonify(data)  # Retourne un JSON
 
 
